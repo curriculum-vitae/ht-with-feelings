ce93fb7ab3b90ac695f47944c3560d9e9c216a4a
Rohov <rogov.dmitry@gmail.com>
2018-12-25 18:00:37 +0200

    ➕

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+import { compose, withState } from "recompose";
+
 const HABITS = [
   {
     name: "Drinking water"
@@ -26,54 +28,110 @@ const HABITS = [
   }
 ];
 
-const Feelings = () => (
+const FEELINGS = [`🥳`, `😁`, `😐`, `😢`, `🙁`];
+
+const Feelings = ({ selected = [], onChange }) => (
   <span
     aria-label={"feelings"}
     role={"img"}
     style={{
-      opacity: "0.8",
       display: "block",
-      fontSize: "16px"
+      fontSize: "28px"
     }}
   >
-    🥳 😁 😐 😢 🙁
+    {FEELINGS.map(icon => (
+      <span
+        style={{
+          cursor: "pointer",
+          opacity: selected.includes(icon) ? "1" : "0.5",
+          padding: "0px 4px"
+        }}
+        onClick={() => {
+          onChange([...selected, icon]);
+        }}
+        key={icon}
+      >
+        {icon}
+      </span>
+    ))}
   </span>
+);
+
+const List = compose(withState("feelings", "setFeelings", {}))(
+  ({ feelings, setFeelings }) => (
+    <div className="App">
+      <header className="App-header">
+        <div
+          style={{
+            maxWidth: "600px",
+            width: "100%",
+            marginLeft: "auto",
+            marginRight: "auto",
+            backgroundColor: "black",
+            minHeight: "100vh"
+          }}
+        >
+          <div
+            style={{
+              height: "80px",
+              textAlign: "center"
+            }}
+          >
+            {Object.keys(feelings).length === 0 &&
+            feelings.constructor === Object ? (
+              <img src={logo} className="App-logo" alt="logo" />
+            ) : (
+              <button
+                onClick={() => {
+                  setFeelings({});
+                  window.alert("Success!");
+                }}
+                style={{
+                  marginTop: "8px",
+                  width: "100%",
+                  border: "2px solid red",
+                  color: "white",
+                  fontSize: "20px",
+                  backgroundColor: "unset",
+                  borderRadius: "4px",
+                  padding: "12px"
+                }}
+              >
+                Submit
+              </button>
+            )}
+          </div>
+          {HABITS.map(habit => (
+            <div
+              key={habit.name}
+              style={{
+                textAlign: "center",
+                border: "1px solid grey",
+                fontSize: "32px",
+                padding: "20px 20px"
+              }}
+            >
+              {habit.name}
+              <Feelings
+                selected={feelings[habit.name]}
+                onChange={changes => {
+                  setFeelings({
+                    ...feelings,
+                    [habit.name]: changes
+                  });
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  )
 );
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <div
-            style={{
-              maxWidth: "600px",
-              width: "100%",
-              marginLeft: "auto",
-              marginRight: "auto",
-              backgroundColor: "black",
-              minHeight: "100vh"
-            }}
-          >
-            <img src={logo} className="App-logo" alt="logo" />
-            {HABITS.map(habit => (
-              <div
-                key={habit.name}
-                style={{
-                  height: "100px",
-                  textAlign: "center",
-                  border: "1px solid grey",
-                  padding: "16px"
-                }}
-              >
-                {habit.name}
-                <Feelings />
-              </div>
-            ))}
-          </div>
-        </header>
-      </div>
-    );
+    return <List />;
   }
 }
 
