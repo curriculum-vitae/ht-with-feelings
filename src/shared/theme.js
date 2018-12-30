122a8a1574d718379a8d9acea456d3dcd018d603
Rohov <rogov.dmitry@gmail.com>
2018-12-30 16:47:31 +0200

    🎨

--- src/shared/theme.js
+++ src/shared/theme.js
@@ -3,26 +3,22 @@ import { teal, red } from "@material-ui/core/colors";
 
 export default createMuiTheme({
   palette: {
-    /*
     type: "dark",
-    */
     background: {
-      /*
       default: "black",
       paper: "#111"
-      */
-    }
+    },
+    primary: teal,
+    secondary: red
   },
   typography: {
     useNextVariants: true
   },
   overrides: {
-    /*
     MuiAppBar: {
       colorPrimary: {
         backgroundColor: "black"
       }
     }
-    */
   }
 });
