9cbc578ee030859ef1b15afd5a509874008d2613
Rohov <rogov.dmitry@gmail.com>
2019-01-01 21:47:24 +0200

    ➕

--- src/screens/Habit/components/HabitAppBar.js
+++ src/screens/Habit/components/HabitAppBar.js
@@ -1,11 +1,25 @@
 import React from "react";
 import { AppBar, Toolbar, Icon, IconButton } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { FirebaseContext } from "contexts/FirebaseContext";
 
-export const HabitAppBar = () => (
+export const HabitAppBar = ({ onDelete }) => (
   <Toolbar>
-    <IconButton component={Link} to={"/"}>
-      <Icon>arrow_back</Icon>
-    </IconButton>
+    <div
+      style={{
+        flexGrow: "1"
+      }}
+    >
+      <IconButton component={Link} to={"/"}>
+        <Icon>arrow_back</Icon>
+      </IconButton>
+    </div>
+    <FirebaseContext.Consumer>
+      {db => (
+        <IconButton onClick={onDelete}>
+          <Icon>delete_outline</Icon>
+        </IconButton>
+      )}
+    </FirebaseContext.Consumer>
   </Toolbar>
 );
--- src/screens/Habit/index.js
+++ src/screens/Habit/index.js
@@ -29,8 +29,6 @@ import { FeelingsProvider } from "providers/FeelingsProvider";
 
 export const HabitScreen = ({ match, stats, history }) => (
   <>
-    <HabitAppBar />
-
     <HabitsProvider>
       {props => {
         const habit = flow(
@@ -40,114 +38,110 @@ export const HabitScreen = ({ match, stats, history }) => (
         if (!habit) return null;
 
         return (
-          <div
-            style={{
-              padding: "0px 20px"
-            }}
-          >
-            <Grid container>
-              <Grid item xs={10}>
-                <Typography variant={"h4"} gutterBottom>
-                  {habit.name}
-                </Typography>
-              </Grid>
-
-              <Grid item xs={2}>
-                <FirebaseContext.Consumer>
-                  {db => (
-                    <IconButton
-                      onClick={() => {
-                        db.collection("habits")
-                          .doc(habit.id)
-                          .delete()
-                          .then(() => {
-                            history.goBack();
-                          })
-                          .catch(e => {
-                            window.alert("Error");
-                          });
-                      }}
-                    >
-                      <Icon>delete_outline</Icon>
-                    </IconButton>
-                  )}
-                </FirebaseContext.Consumer>
-              </Grid>
-            </Grid>
-
-            <Grid container alignItems={"flex-end"}>
-              <Grid item xs={6}>
-                <Typography variant={"h6"} gutterBottom>
-                  Overview
-                </Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography
-                  variant={"caption"}
-                  gutterBottom
-                  style={{ textAlign: "right" }}
-                >
-                  {" "}
-                  last 21 day
-                </Typography>
-              </Grid>
-            </Grid>
-            <br />
-
-            <FeelingsProvider idHabit={habit.id}>
-              {props => {
-                const stats = getStats(props.feelings);
-                const statsItems = getStatsItems(stats);
-
-                return (
-                  <Grid container spacing={40}>
-                    {flow(
-                      map(statsItem => (
-                        <Grid
-                          item
-                          xs={4}
-                          style={{ textAlign: "center" }}
-                          key={statsItem.emoji}
-                        >
-                          <div
-                            style={{
-                              fontSize: `${getRandomFontSize()}px`,
-                              height: "80px"
-                            }}
-                          >
-                            {statsItem.emoji}
-                          </div>
-                          <Typography>{statsItem.count}</Typography>
-                        </Grid>
-                      ))
-                    )(statsItems)}
-                  </Grid>
-                );
+          <>
+            <FirebaseContext.Consumer>
+              {db => (
+                <HabitAppBar
+                  onDelete={() => {
+                    db.collection("habits")
+                      .doc(habit.id)
+                      .delete()
+                      .then(() => {
+                        history.goBack();
+                      })
+                      .catch(e => {
+                        window.alert("Error");
+                      });
+                  }}
+                />
+              )}
+            </FirebaseContext.Consumer>
+            <div
+              style={{
+                padding: "0px 20px"
               }}
-            </FeelingsProvider>
+            >
+              <Grid container>
+                <Grid item xs={10}>
+                  <Typography variant={"h4"} gutterBottom>
+                    {habit.name}
+                  </Typography>
+                </Grid>
 
-            <Typography variant={"h6"} gutterBottom>
-              Log
-            </Typography>
-
-            <List dense>
-              {flow(
-                generateFakeStats,
-                map(stat => (
-                  <ListItem key={Math.random()}>
-                    <ListItemAvatar>
-                      <Avatar>{stat.feelings[0]}</Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary={moment(stat.dates[0]).format(
-                        "DD/MM/YYYY, HH:mm"
-                      )}
-                    />
-                  </ListItem>
-                ))
-              )(0)}
-            </List>
-          </div>
+                <Grid item xs={2} />
+              </Grid>
+              <FeelingsProvider idHabit={habit.id}>
+                {props => {
+                  const stats = getStats(props.feelings);
+                  const statsItems = getStatsItems(stats);
+                  return (
+                    <>
+                      <Grid container alignItems={"flex-end"}>
+                        <Grid item xs={6}>
+                          <Typography variant={"h6"} gutterBottom>
+                            Overview
+                          </Typography>
+                        </Grid>
+                        <Grid item xs={6}>
+                          <Typography
+                            variant={"caption"}
+                            gutterBottom
+                            style={{ textAlign: "right" }}
+                          >
+                            {" "}
+                            last 21 day
+                          </Typography>
+                        </Grid>
+                      </Grid>
+                      <br />
+                      <Grid container spacing={40}>
+                        {flow(
+                          map(statsItem => (
+                            <Grid
+                              item
+                              xs={4}
+                              style={{ textAlign: "center" }}
+                              key={statsItem.emoji}
+                            >
+                              <div
+                                style={{
+                                  fontSize: `${getRandomFontSize()}px`,
+                                  height: "80px"
+                                }}
+                              >
+                                {statsItem.emoji}
+                              </div>
+                              <Typography>{statsItem.count}</Typography>
+                            </Grid>
+                          ))
+                        )(statsItems)}
+                      </Grid>
+                    </>
+                  );
+                }}
+              </FeelingsProvider>
+              <Typography variant={"h6"} gutterBottom>
+                Log
+              </Typography>
+              <List dense>
+                {flow(
+                  generateFakeStats,
+                  map(stat => (
+                    <ListItem key={Math.random()}>
+                      <ListItemAvatar>
+                        <Avatar>{stat.feelings[0]}</Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={moment(stat.dates[0]).format(
+                          "DD/MM/YYYY, HH:mm"
+                        )}
+                      />
+                    </ListItem>
+                  ))
+                )(0)}
+              </List>
+            </div>
+          </>
         );
       }}
     </HabitsProvider>
