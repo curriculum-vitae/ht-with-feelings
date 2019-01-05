0c0d445f20299152fe68d9f333a4613a23a8f652
Rohov <rogov.dmitry@gmail.com>
2019-01-05 14:08:03 +0200

    🐞

--- src/screens/Habit/index.js
+++ src/screens/Habit/index.js
@@ -10,88 +10,101 @@ import { HabitEmojiOverview } from "screens/Habit/components/HabitEmojiOverview"
 import { getStats, getStatsItems } from "screens/Habit/helpers";
 //
 import { HabitEmojiList } from "screens/Habit/components/HabitEmojiList";
+import { AuthObserver } from "features/AuthObserver";
 
 export const HabitScreen = ({ match, history }) => (
-  <>
-    <HabitsProvider>
-      {props => {
-        const habit = flow(
-          props => props.habits,
-          find(habit => habit.id === match.params.idHabit)
-        )(props);
-        if (!habit) return null;
+  <AuthObserver>
+    {({ isSignedIn }) =>
+      isSignedIn ? (
+        <HabitsProvider>
+          {props => {
+            const habit = flow(
+              props => props.habits,
+              find(habit => habit.id === match.params.idHabit)
+            )(props);
+            if (!habit) return null;
 
-        const deleteHabit = db => {
-          db.collection("habits")
-            .doc(habit.id)
-            .delete()
-            .then(() => {
-              history.goBack();
-            })
-            .catch(e => {
-              window.alert("Error");
-            });
-        };
-        return (
-          <>
-            <FirebaseContext.Consumer>
-              {db => <HabitAppBar onDelete={() => deleteHabit(db)} />}
-            </FirebaseContext.Consumer>
-            <div
-              style={{
-                padding: "0px 20px"
-              }}
-            >
-              <Typography variant={"h4"} gutterBottom>
-                {habit.name}
-              </Typography>
-
-              <FeelingsProvider idHabit={habit.id}>
-                {props => {
-                  return (
+            const deleteHabit = db => {
+              db.collection("habits")
+                .doc(habit.id)
+                .delete()
+                .then(() => {
+                  history.goBack();
+                })
+                .catch(e => {
+                  window.alert("Error");
+                });
+            };
+            return (
+              <AuthObserver>
+                {({ isSignedIn }) =>
+                  isSignedIn ? (
                     <>
-                      <Typography variant={"h6"} gutterBottom>
-                        Overview
-                      </Typography>
-                      <br />
-                      <HabitEmojiOverview
-                        statsItems={flow(
-                          props => props.feelings,
-                          getStats,
-                          getStatsItems
-                        )(props)}
-                      />
-                    </>
-                  );
-                }}
-              </FeelingsProvider>
-              <Typography variant={"h6"} gutterBottom>
-                Log
-              </Typography>
-              <FeelingsProvider idHabit={habit.id}>
-                {props => (
-                  <FirebaseContext.Consumer>
-                    {db => (
-                      <HabitEmojiList
-                        records={props.feelings}
-                        onDelete={({ record, emoji }) => {
-                          db.collection("records")
-                            .doc(record.id)
-                            .update({
-                              feelings: firebase.firestore.FieldValue.arrayRemove(
-                                emoji
-                              )
-                            });
+                      <FirebaseContext.Consumer>
+                        {db => <HabitAppBar onDelete={() => deleteHabit(db)} />}
+                      </FirebaseContext.Consumer>
+                      <div
+                        style={{
+                          padding: "0px 20px"
                         }}
-                      />
-                    )}
-                  </FirebaseContext.Consumer>
-                )}
-              </FeelingsProvider>
-            </div>
-          </>
-        );
-      }}
-    </HabitsProvider>
-  </>
+                      >
+                        <Typography variant={"h4"} gutterBottom>
+                          {habit.name}
+                        </Typography>
+
+                        <FeelingsProvider idHabit={habit.id}>
+                          {props => {
+                            return (
+                              <>
+                                <Typography variant={"h6"} gutterBottom>
+                                  Overview
+                                </Typography>
+                                <br />
+                                <HabitEmojiOverview
+                                  statsItems={flow(
+                                    props => props.feelings,
+                                    getStats,
+                                    getStatsItems
+                                  )(props)}
+                                />
+                              </>
+                            );
+                          }}
+                        </FeelingsProvider>
+                        <Typography variant={"h6"} gutterBottom>
+                          Log
+                        </Typography>
+                        <FeelingsProvider idHabit={habit.id}>
+                          {props => (
+                            <FirebaseContext.Consumer>
+                              {db => (
+                                <HabitEmojiList
+                                  records={props.feelings}
+                                  onDelete={({ record, emoji }) => {
+                                    db.collection("records")
+                                      .doc(record.id)
+                                      .update({
+                                        feelings: firebase.firestore.FieldValue.arrayRemove(
+                                          emoji
+                                        )
+                                      });
+                                  }}
+                                />
+                              )}
+                            </FirebaseContext.Consumer>
+                          )}
+                        </FeelingsProvider>
+                      </div>
+                    </>
+                  ) : null
+                }
+              </AuthObserver>
+            );
+          }}
+        </HabitsProvider>
+      ) : (
+        "Please sign in"
+      )
+    }
+  </AuthObserver>
 );
