d8151f45c085b7d9e007623ca237582acafff63c
Rohov <rogov.dmitry@gmail.com>
2019-01-10 17:21:00 +0200

    ➕

--- scripts/create-habits.js
+++ scripts/create-habits.js
@@ -18,7 +18,8 @@ firebase.initializeApp(config);
 
 const db = firebase.firestore();
 
-const MY_USER_ID = "elYllDGPEAcPpnBAgJkOyPLGdW02";
+const ID_ME = "elYllDGPEAcPpnBAgJkOyPLGdW02";
+const ID_MYROSIA = "FjxH8CtpSTNItsM35mOr1R12UYV2";
 // Disable deprecated features
 db.settings({
   timestampsInSnapshots: true
@@ -50,76 +51,84 @@ const LISTS = [
 ];
 
 const HABBITS = [
-  ["Drink more water", ["consumpting"]],
-  ["Avoid leg locking", ["sitting", "coding"]],
-  ["No web after 24:00", ["sleeping"]],
-  ["Prepare for the next day", ["planning"]],
-  ["Perfect week planning", ["planning"]],
-  ["Write to Notion", ["learning"]],
-  ["Use Pomodoro", ["coding"]],
-  ["No dancing after 1:00", ["dancing"]],
-  ["Do misc exercising", ["exercising"]],
-  ["Do the plank", ["exercising"]],
-  ["Do morning routine", ["waking"]],
-  ["Do evening routine", ["sleeping"]],
-  ["Call Pa", ["communicating"]],
-  ["Avoid head bending", ["dancing"]],
-  ["Use malatonin", ["sleeping"]],
-  ["Avoid coffee after 18:00", ["sleeping"]],
-  ["Avoid wasting morning time in bed", ["waking"]],
-  ["Avoid wasting evening time in bed", ["sleeping"]],
-  ["Eat nicely", ["consumpting"]],
-  ["Avoid distractions", ["coding"]],
-  ["Prepare for teaching", ["planning", "teaching"]],
-  ["Reset the kithen", ["housing"]],
-  ["Avoid social networks", ["sitting"]],
-  ["Listen more", ["communicating"]],
-  ["Listen more to podcasts", ["sleeping"]],
-  ["Brush teeth", ["sleeping"]],
-  ["Always understand what I am working on", ["coding"]],
-  ["Prefer learning from individuals", ["learning"]]
+  ["Drink more water", ["consumpting"], [ID_ME, ID_MYROSIA]],
+  ["Avoid leg locking", ["sitting", "coding"], [ID_ME]],
+  ["No web after 24:00", ["sleeping"], [ID_ME, ID_MYROSIA]],
+  ["Prepare for the next day", ["planning"], [ID_ME, ID_MYROSIA]],
+  ["Perfect week planning", ["planning"], [ID_ME]],
+  ["Write to Notion", ["learning"], [ID_ME]],
+  ["Use Pomodoro", ["coding"], [ID_ME]],
+  ["No dancing after 1:00", ["dancing"], [ID_ME]],
+  ["Do exercising", ["exercising"], [ID_ME, ID_MYROSIA]],
+  ["Do the plank", ["exercising"], [ID_ME]],
+  ["Do morning routine", ["waking"], [ID_ME]],
+  ["Do evening routine", ["sleeping"], [ID_ME]],
+  ["Call Pa", ["communicating"], [ID_ME]],
+  ["Avoid head bending", ["dancing"], [ID_ME]],
+  ["Use malatonin", ["sleeping"], [ID_ME]],
+  ["Avoid coffee after 18:00", ["sleeping"], [ID_ME]],
+  ["Avoid wasting morning time in bed", ["waking"], [ID_ME]],
+  ["Avoid wasting evening time in bed", ["sleeping"], [ID_ME]],
+  ["Eat nicely", ["consumpting"], [ID_ME, ID_MYROSIA]],
+  ["Avoid distractions", ["coding"], [ID_ME]],
+  ["Prepare for teaching", ["planning", "teaching"], [ID_ME]],
+  ["Reset the kithen", ["housing"], [ID_ME, ID_MYROSIA]],
+  ["Avoid social networks", ["sitting"], [ID_ME]],
+  ["Listen more", ["communicating"], [ID_ME]],
+  ["Listen more to podcasts", ["sleeping"], [ID_ME]],
+  ["Brush teeth", ["sleeping"], [ID_ME, ID_MYROSIA]],
+  ["Always understand what I am working on", ["coding"], [ID_ME]],
+  ["Prefer learning from individuals", ["learning"], [ID_ME]]
 ];
 
-const exec = async () => {
+const createLists = async () => {
+  const lists = flow(
+    map(habit => habit[1]),
+    flatten,
+    uniq
+  )(HABBITS);
+
+  return await flow(
+    map(list =>
+      db
+        .collection("lists")
+        .doc(list)
+        .set({
+          name: list,
+          uid: ID_ME,
+          position: LISTS.indexOf(list)
+        })
+    ),
+    req => Promise.all(req)
+  )(lists);
+};
+
+const createHabits = async () => {
+  return await flow(
+    sortBy(habit => habit[0]),
+    map(habit =>
+      db
+        .collection("habits")
+        .doc(convertNameToID(habit[0]))
+        .set({
+          name: habit[0],
+          uid: ID_ME,
+          uids: habit[2],
+          lists: map(list => db.doc(`lists/${list}`))(habit[1])
+        })
+    ),
+    requests => Promise.all(requests)
+  )(HABBITS);
+};
+
+const createAll = async () => {
   try {
-    const lists = flow(
-      map(habit => habit[1]),
-      flatten,
-      uniq
-    )(HABBITS);
-
-    await flow(
-      map(list =>
-        db
-          .collection("lists")
-          .doc(list)
-          .set({
-            name: list,
-            uid: MY_USER_ID,
-            position: LISTS.indexOf(list)
-          })
-      ),
-      req => Promise.all(req)
-    )(lists);
-
-    await flow(
-      sortBy(habit => habit[0]),
-      map(habit =>
-        db
-          .collection("habits")
-          .doc(convertNameToID(habit[0]))
-          .set({
-            name: habit[0],
-            uid: MY_USER_ID,
-            lists: map(list => db.doc(`lists/${list}`))(habit[1])
-          })
-      ),
-      requests => Promise.all(requests)
-    )(HABBITS);
+    await createLists();
+    await createHabits();
     console.log("Document successfully written!");
   } catch (e) {
     console.error("Error writing document: ", e);
   }
 };
 
-exec();
+createAll();
