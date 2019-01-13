826958811eff78b29a45f0443d8cefbeb113d3e8
Rohov <rogov.dmitry@gmail.com>
2019-01-13 12:03:19 +0200

    ➕

--- src/screens/Index/components/IndexHabitsList.js
+++ src/screens/Index/components/IndexHabitsList.js
@@ -91,7 +91,7 @@ export const IndexHabitsList = ({ habits, date, records }) => {
                   return up;
                 }, {});
                 return (
-                  <Grid item xs={6} key={habit.id}>
+                  <Grid item xs={12} key={habit.id}>
                     <Link to={`/habits/${habit.id}`}>
                       <IndexHabitsListItemV3
                         habit={habit}
