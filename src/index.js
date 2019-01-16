ad5fda97b67af6f84ea5d33b1b4680e67b0f091f
Rohov <rogov.dmitry@gmail.com>
2019-01-16 13:22:19 +0200

    ➕

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from "react-dom";
 import "index.css";
 import App from "App.js";
 import * as serviceWorker from "serviceWorker.js";
+import ReactGA from "react-ga";
+import { history } from "misc/history";
+
+if (process.env.NODE_ENV === "production") {
+  ReactGA.initialize("UA-49161653-4");
+  history.listen(() => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
